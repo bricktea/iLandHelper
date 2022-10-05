9fab5d693601852071b72461d068ddfcaa17a43d
Redbeanw44602 <redbeana44945@gmail.com>
2022-10-05 13:07:39 +0800

    Initial commit

---
+++ src/Plugin.cpp
@@ -0,0 +1,98 @@
+#include "Plugin.h"
+
+#include <llapi/EventAPI.h>
+
+using namespace std;
+using namespace ll;
+Logger logger("iLandHelper");
+
+void PluginMain() {
+    logger.info("plugin loaded!");
+}
+
+void PluginInit() {
+    registerPlugin("iLandHelper", "Helper for iland.",
+                   Version{1, 0, 0, Version::Release}, {{"Author", "Redbeanw"}});
+    Event::ServerStartedEvent::subscribe([](Event::ServerStartedEvent ev) -> bool {
+        PluginMain();
+        return true;
+    });
+}
+
+#include "llapi/RemoteCallAPI.h"
+#include "llapi/mc/BlockSource.hpp"
+
+static auto PosGetLand = RemoteCall::importAs<string(unordered_map<string,int>,bool)>("LLSEGlobal","ILAPI_PosGetLand");
+static auto GetLandInRange = RemoteCall::importAs<vector<string>(std::unordered_map<string,int>,unordered_map<string,int>,int,bool)>("LLSEGlobal","ILAPI_GetLandInRange");
+
+class ILand {
+public:
+    static std::optional<string> getPos(BlockSource* region,BlockPos pos);
+    static std::optional<vector<string>> getAABB(BlockSource* region, BlockPos min, BlockPos max);
+
+};
+
+std::optional<string> ILand::getPos(BlockSource *region, BlockPos pos) {
+    try {
+        return PosGetLand(unordered_map<string,int>{
+                {"x",pos.x},
+                {"y",pos.y},
+                {"z",pos.z},
+                {"dimid",region->getDimensionId()}
+        },false);
+    }
+    catch (...)
+    {
+        return std::nullopt;
+    }
+}
+
+std::optional<vector<string>> ILand::getAABB(BlockSource *region, BlockPos min, BlockPos max) {
+    try {
+        return GetLandInRange(
+                unordered_map<string,int>{
+                        {"x",min.x},
+                        {"y",min.y},
+                        {"z",min.z}},
+                unordered_map<string,int>{
+                        {"x",max.x},
+                        {"y",max.y},
+                        {"z",max.z}},
+                region->getDimensionId(),
+                false);
+
+    }
+    catch (...)
+    {
+        return std::nullopt;
+    }
+}
+
+#include "llapi/mc/SculkCatalystBlockActor.hpp"
+TInstanceHook(void, "?_tryConsumeOnDeathExperience@SculkCatalystBlockActor@@AEAAXAEAVLevel@@AEAVActor@@@Z",
+              SculkCatalystBlockActor, Level * a2, Actor * a3)
+{
+    auto region = &a3->getRegion();
+    auto center = a3->getBlockStandingOn().getPosition();
+    auto currentLand = ILand::getPos(region,center);
+    if (!currentLand)
+    {
+        auto nearbyLands = ILand::getAABB(region, center - 2,center + 2);
+        if (nearbyLands && !nearbyLands->empty())
+            return;
+    }
+    original(this, a2, a3);
+}
+
+TClasslessInstanceHook(char, "?onFertilized@MossBlock@@UEBA_NAEAVBlockSource@@AEBVBlockPos@@PEAVActor@@W4FertilizerType@@@Z",
+                       BlockSource* bs, BlockPos bp, Actor* ac, FertilizerType type)
+{
+    auto currentLand = ILand::getPos(bs,bp);
+    if (!currentLand)
+    {
+        auto nearbyLands = ILand::getAABB(bs, bp - 2,bp + 2);
+        if (nearbyLands && !nearbyLands->empty())
+            return 0;
+    }
+    return original(this, bs, bp, ac, type);
+}
---
+++ src/Plugin.h
@@ -0,0 +1,8 @@
+#ifndef PLUGIN_H
+#define PLUGIN_H
+
+#include <iostream>
+#include <llapi/Global.h>
+#include <llapi/LLAPI.h>
+
+#endif // PLUGIN_H
